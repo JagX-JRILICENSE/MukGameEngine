d6ef4911b095e77cb5a74fd21a744ac461955d46
JagX-JRILICENSE <jrilicense@gmail.com>
2026-09-08 00:46:34 +0100

    Add Matrix.h with basic transforms

---
+++ Engine/Math/Matrix.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Vector.h"
+
+namespace Muk {
+
+struct Mat4 {
+    f32 m[16] = {
+        1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        0,0,0,1
+    };
+
+    Mat4() = default;
+
+    static Mat4 Identity() { return Mat4{}; }
+
+    static Mat4 Translation(const Vec3& t) {
+        Mat4 result;
+        result.m[12] = t.x;
+        result.m[13] = t.y;
+        result.m[14] = t.z;
+        return result;
+    }
+
+    static Mat4 Scale(const Vec3& s) {
+        Mat4 result;
+        result.m[0] = s.x;
+        result.m[5] = s.y;
+        result.m[10] = s.z;
+        return result;
+    }
+
+    // Perspective projection (simple)
+    static Mat4 Perspective(f32 fovY, f32 aspect, f32 nearZ, f32 farZ) {
+        Mat4 result{};
+        f32 tanHalfFovy = std::tan(fovY * 0.5f);
+        result.m[0] = 1.0f / (aspect * tanHalfFovy);
+        result.m[5] = 1.0f / tanHalfFovy;
+        result.m[10] = -(farZ + nearZ) / (farZ - nearZ);
+        result.m[11] = -1.0f;
+        result.m[14] = -(2.0f * farZ * nearZ) / (farZ - nearZ);
+        result.m[15] = 0.0f;
+        return result;
+    }
+};
+
+} // namespace Muk
