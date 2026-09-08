f4f3a151719aadc004a1d5f04fb991bc79b6739f
JagX-JRILICENSE <jrilicense@gmail.com>
2026-09-08 05:51:42 +0100

    Update Engine.h to expose all new modules

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -15,37 +15,8 @@
 #include "ECS/Component.h"
 #include "RHI/RHI.h"
 #include "Renderer/Renderer.h"
+#include "Renderer/Mesh.h"
+#include "Renderer/Material.h"
 #include "Input/Input.h"
-
-namespace Muk {
-
-class Engine {
-public:
-    struct Config {
-        const char* windowTitle = "Muk Game Engine";
-        int width = 1280;
-        int height = 720;
-        bool fullscreen = false;
-        bool vsync = true;
-    };
-
-    Engine() = default;
-    ~Engine();
-
-    bool Initialize(const Config& config);
-    void Run();
-    void Shutdown();
-
-    World& GetWorld() { return m_World; }
-    Window& GetWindow() { return m_Window; }
-    Renderer& GetRenderer() { return m_Renderer; }
-
-private:
-    bool m_Running = false;
-    Window m_Window;
-    World m_World;
-    Renderer m_Renderer;
-    Input m_Input;
-};
-
-} // namespace Muk
+#include "Physics/PhysicsWorld.h"
+#include "Asset/AssetManager.h"
