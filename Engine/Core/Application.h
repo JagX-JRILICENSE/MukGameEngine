9c40253d1410630a7c4b938b4de0adad77c9f8ec
JagX-JRILICENSE <jrilicense@gmail.com>
2026-09-08 05:50:37 +0100

    Integrate Renderer, Physics, AssetManager into Application

--- Engine/Core/Application.h
+++ Engine/Core/Application.h
@@ -2,6 +2,10 @@
 
 #include "Core.h"
 #include "Window.h"
+#include "Renderer/Renderer.h"
+#include "Physics/PhysicsWorld.h"
+#include "Asset/AssetManager.h"
+#include "ECS/World.h"
 
 namespace Muk {
 
@@ -14,6 +18,11 @@ public:
     void Close();
 
     Window& GetWindow() { return m_Window; }
+    Renderer& GetRenderer() { return m_Renderer; }
+    PhysicsWorld& GetPhysics() { return m_Physics; }
+    AssetManager& GetAssets() { return m_Assets; }
+    World& GetWorld() { return m_World; }
+
     static Application& Get() { return *s_Instance; }
 
 protected:
@@ -22,8 +31,20 @@ protected:
     virtual void OnRender() {}
     virtual void OnShutdown() {}
 
+    // Convenience accessors for derived classes
+    Window& Window() { return m_Window; }
+    Renderer& Renderer() { return m_Renderer; }
+    PhysicsWorld& Physics() { return m_Physics; }
+    AssetManager& Assets() { return m_Assets; }
+    World& ECS() { return m_World; }
+
 private:
     Window m_Window;
+    Renderer m_Renderer;
+    PhysicsWorld m_Physics;
+    AssetManager m_Assets;
+    World m_World;
+
     bool m_Running = true;
     static Application* s_Instance;
 };
