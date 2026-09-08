30570043352f4e4ce07ac67f6927fc4e9c5faa91
JagX-JRILICENSE <jrilicense@gmail.com>
2026-09-08 07:15:48 +0100

    Basic.hlsl: sample albedo texture with BaseColor

--- Engine/Renderer/Shaders/Basic.hlsl
+++ Engine/Renderer/Shaders/Basic.hlsl
@@ -1,10 +1,13 @@
-// Muk Game Engine - Basic color shader
-// Vertex color passthrough with simple MVP transform
-
 cbuffer FrameConstants : register(b0)
 {
     float4x4 MVP;
+    float4   BaseColor;
+    float    UseTexture;
+    float3   Pad;
 };
+
+Texture2D    AlbedoTex : register(t0);
+SamplerState AlbedoSam : register(s0);
 
 struct VSInput
 {
@@ -18,6 +21,7 @@ struct PSInput
 {
     float4 Position : SV_POSITION;
     float4 Color    : COLOR;
+    float2 TexCoord : TEXCOORD;
 };
 
 PSInput VSMain(VSInput input)
@@ -25,10 +29,12 @@ PSInput VSMain(VSInput input)
     PSInput output;
     output.Position = mul(float4(input.Position, 1.0f), MVP);
     output.Color = input.Color;
+    output.TexCoord = input.TexCoord;
     return output;
 }
 
 float4 PSMain(PSInput input) : SV_TARGET
 {
-    return input.Color;
+    float4 tex = AlbedoTex.Sample(AlbedoSam, input.TexCoord);
+    return lerp(input.Color * BaseColor, tex * BaseColor, UseTexture);
 }
